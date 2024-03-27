68ad67498d33649aac95a9e480be6b384c43efc7
Crostrix <51205094+Crostrix@users.noreply.github.com>
2024-03-27 17:58:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-sudo pacman -S --needed spotify-launcher dolphin-emu obs-studio git vlc kate virt-manager arch-keyring qemu-base virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat steam wine wget plasma pipewire htop firefox grub efibootmgr sof-firmware
-grub-install --efi-directory=/boot
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo pacman -S --needed spotify-launcher dolphin-emu obs-studio git vlc kate virt-manager arch-keyring qemu-base virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat steam wine wget plasma pipewire htop firefox grub efibootmgr sof-firmware networkmanager docker sddm
+sudo grub-install --efi-directory=/boot
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo systemctl enable sddm
+sudo systemctl enable docker
+sudo systemctl enable virtqemud
