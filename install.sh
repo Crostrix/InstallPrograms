ee1b3966a5bfacd50b1d86edd94aecc867270a1b
Crostrix <51205094+Crostrix@users.noreply.github.com>
2024-03-27 17:44:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S --needed spotify-launcher dolphin-emu obs-studio git vlc kate virt-manager arch-keyring qemu virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat steam wine wget plasma pipewire
+sudo pacman -S --needed spotify-launcher dolphin-emu obs-studio git vlc kate virt-manager arch-keyring qemu virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat steam wine wget plasma pipewire htop firefox
